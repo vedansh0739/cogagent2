c7c9cdc9f5dadefcd5e4867204a1c717a144ce28
Vedansh Bagadia <93564155+vedansh0739@users.noreply.github.com>
2023-12-28 17:21:14 +0530

    Update cli_demo_sat.py

--- basic_demo/cli_demo_sat.py
+++ basic_demo/cli_demo_sat.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
+#ola
 import torch
 import argparse
 from sat.model.mixins import CachedAutoregressiveMixin
